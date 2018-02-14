663b21d8fd07f5919aedc6fdfa3f7ab182b8ce48
Rocciescobar <roccisen.88@gmail.com>
2018-02-14 17:41:36 -0500

    Añade maquetado principal e imágenes random

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+// Obtener 9 imágenes random
+
+let container = document.querySelector('.img-container-js');
+for (let i = 0; i < 9; i++) {  
+  let numberRandom = Math.floor((Math.random() * 87) + 1);
+  const images = `https://starwars-visualguide.com/assets/img/characters/${numberRandom}.jpg`;
+  
+  let newImage = document.createElement('img');
+  newImage.setAttribute('src', images);
+  newImage.setAttribute('class', 'img-thumbnail');
+  newImage.setAttribute('class', 'img-style');
+  newImage.setAttribute('draggable', 'true');
+  newImage.setAttribute('id', numberRandom);
+  container.appendChild(newImage);
+};
