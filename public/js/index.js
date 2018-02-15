b96bc4866b88d5aea2925207e5aad2a76aba279d
Manuela <fmanuela499@gmail.com>
2018-02-15 03:48:44 -0500

    funcion que trae los datos

--- public/js/index.js
+++ public/js/index.js
@@ -15,3 +15,50 @@ for (var i = 0; i < 9; i++) {
   newImage.setAttribute('id', numberRandom);
   container.appendChild(newImage);
 };
+// función búsqueda : 
+var searchButton = $('.search-js');
+var input = $('.input-js');
+console.log(input);
+console.log(searchButton);
+
+searchButton.on('click', function () {
+  console.log('click');
+  var valueInput = input.val();
+  console.log(valueInput);
+  // funcion para imprimir los personajes : 
+  var getInfo = function getInfo() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var data = JSON.parse(this.responseText);
+        var result = data.results;
+        console.log(result);
+        var long = data.results.length;
+        console.log(long);
+        xhr.onload = printFigure(result);
+        xhr.onerror = handleError;
+      };
+    };
+    xhr.open('GET', 'https://swapi.co/api/people/');
+    xhr.send();
+  };
+  console.log(getInfo());
+  var printFigure = function printFigure(data) {
+    console.log(data);
+    data.forEach(function (element, index) {
+      var image = 'https://starwars-visualguide.com/assets/img/characters/' + (index + 1) + '.jpg';
+      var newImage = document.createElement('img');
+      newImage.setAttribute('src', image);
+      newImage.setAttribute('name', element.name);
+      newImage.setAttribute('class', 'img-thumbnail');
+      newImage.setAttribute('class', 'img-style');
+      // newImage.setAttribute('draggable', 'true');
+      newImage.setAttribute('id', index + 1);
+      container.appendChild(newImage);
+    });
+  };
+
+  var handleError = function handleError(error) {
+    console.log(error);
+  };
+});
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // Obtener 9 imágenes random
 
 let container = document.querySelector('.img-container-js');
-for (let i = 0; i < 9; i++) {  
+for (let i = 0; i < 9; i++) {
   let numberRandom = Math.floor((Math.random() * 87) + 1);
   const images = `https://starwars-visualguide.com/assets/img/characters/${numberRandom}.jpg`;
-  
+
   let newImage = document.createElement('img');
   newImage.setAttribute('src', images);
   newImage.setAttribute('class', 'img-thumbnail');
@@ -13,3 +13,49 @@ for (let i = 0; i < 9; i++) {
   newImage.setAttribute('id', numberRandom);
   container.appendChild(newImage);
 };
+// función que trae los datos : 
+const searchButton = $('.search-js');
+const input = $('.input-js');
+console.log(input);
+console.log(searchButton);
+
+searchButton.on('click', () => {
+  console.log('click');
+  let valueInput = input.val();
+  console.log(valueInput);
+  // funcion para imprimir los personajes : 
+  let getInfo = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const data = JSON.parse(this.responseText);
+        let result = data.results;
+        console.log(result);
+        let long = data.results.length;
+        console.log(long);
+        xhr.onload = printFigure(result);
+        xhr.onerror = handleError;
+      };
+    };
+    xhr.open('GET', 'https://swapi.co/api/people/');
+    xhr.send();
+  };
+  console.log(getInfo());
+  let printFigure = (data) => {
+    console.log(data);
+    data.forEach((element, index) => {
+      let image = `https://starwars-visualguide.com/assets/img/characters/${index + 1}.jpg`;
+      let newImage = document.createElement('img');
+      newImage.setAttribute('src', image);
+      newImage.setAttribute('name', element.name);
+      newImage.setAttribute('class', 'img-thumbnail');
+      newImage.setAttribute('class', 'img-style');
+      newImage.setAttribute('id', index + 1);
+      container.appendChild(newImage);
+    });
+  };
+
+  let handleError = error => {
+    console.log(error);
+  };
+});
